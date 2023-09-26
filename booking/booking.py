ccb303005ac505af3573cdf4abeec01a1d3f6d13
Louis Painter <louis.painter@imt-atlantique.net>
2023-09-26 18:01:22 +0200

    Ajout routes booking

--- booking/booking.py
+++ booking/booking.py
@@ -15,6 +15,36 @@ with open('{}/databases/bookings.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
+@app.route("/bookings", methods=['GET'])
+def get_json():   
+   print(bookings)
+   return make_response(jsonify(bookings), 200)
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_for_user(userid: str):
+   
+   for booking in bookings:
+      if str(booking["userid"]) == str(userid):
+            res = make_response(jsonify(booking),200)
+            return res
+   return make_response(jsonify({"error":"User ID not found"}),400)
+
+@app.route("/bookings/<userid>", methods=['POST'])
+def add_booking_byuser(userid: str):
+   req = request.get_json()
+
+   for booking in bookings:
+      if str(booking["userid"]) == str(userid):
+         for date in booking["dates"]:
+            if str(date["date"]) == str(req["date"]):
+               date["movies"].append(req["movieid"])
+               return make_response(jsonify({"message":"booking added"}),200)
+   
+         booking["dates"].append({"date": str(req["date"]), "movies": [str(req["movieid"])]})
+         return make_response(jsonify({"message":"booking added"}),200)
+   return make_response(jsonify({"error":"User ID not found"}),400)
+
+
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
